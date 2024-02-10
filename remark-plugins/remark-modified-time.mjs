14916e50f4192a1807d6bd6dbace4f6163d0e3e1
David Maciejewski <macx@macx.io>
2024-02-10 14:13:50 +0100

    TOC, GitHub-Action, Content

--- remark-plugins/remark-modified-time.mjs
+++ remark-plugins/remark-modified-time.mjs
@@ -1,9 +1,11 @@
-import { execSync } from "child_process";
+import { execSync } from 'child_process'
 
 export function remarkModifiedTime() {
   return function (tree, file) {
-    const filepath = file.history[0];
-    const result = execSync(`git log -1 --format=%cd --date=short ${filepath}`);
-    file.data.astro.frontmatter.lastModified = result.toString().trim();
-  };
+    if (typeof tree === 'undefined') throw new Error('Missing tree')
+
+    const filepath = file.history[0]
+    const result = execSync(`git log -1 --format=%cd --date=short ${filepath}`)
+    file.data.astro.frontmatter.lastModified = result.toString().trim()
+  }
 }
