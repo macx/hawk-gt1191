197cd9ffd66213875710cdfb8b38ce42d4218f32
David Maciejewski <macx@macx.io>
2024-02-12 18:18:27 +0100

    Update dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,7 +43,7 @@ export default defineConfig({
     icon({
       include: {
         materialSymbols: ['menu', 'close', 'arrow-upward-rounded'],
-        simpleIcons: ['discord']
+        simpleIcons: ['discord', 'github']
       },
       svgoOptions: {
         plugins: [
