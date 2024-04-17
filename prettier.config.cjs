a39c0f38b4520b5f5663d164e67091ff43c0ea61
David Maciejewski <macx@macx.io>
2024-04-17 08:21:27 +0200

    chore: Update dependencies

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   ...require('prettier-config-standard'),
-  pluginSearchDirs: [__dirname],
   plugins: [require.resolve('prettier-plugin-astro')],
   overrides: [
     {
