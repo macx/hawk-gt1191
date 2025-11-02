10a6b7e17bb55bebc36bf08bd2545f1b394a446e
David Maciejewski <macx@macx.io>
2025-11-02 12:33:23 +0100

    remove cssnano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,5 @@
 // @ts-nocheck
 module.exports = {
   syntax: 'postcss-scss',
-  plugins: [
-    require('autoprefixer'),
-    require('cssnano')({
-      preset: 'default'
-    })
-  ]
+  plugins: [require('autoprefixer')]
 }
